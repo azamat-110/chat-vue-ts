36652640e0780fc73255e0598d42ed2c639ae1aa
azamat-110 <tursunbayevazamat565@gmail.com>
2025-06-24 17:50:37 +0500

    fix

--- src/components/IncomeMessage.vue
+++ src/components/IncomeMessage.vue
@@ -7,12 +7,18 @@ const messageTextClass =
 		: 'income-message'
 const sendMessageClass = props.messageData.userId == props.userId ? 'end' : ''
 const messageImageClass = props.messageData.imgUrl ? 'with-img' : 'without-img'
+
+let mins: number = Math.round(props.messageData.messageTime / 60000)
+let hours: number = Math.floor(mins / 60)
+mins %= 60
+hours %= 24
+const solveMessageTime = hours + 5 + ':' + mins
 </script>
 
 <template>
 	<div class="send-message" :class="sendMessageClass">
 		<div v-if="messageData.userId == userId" class="message-time">
-			{{ messageData.messageTime }}
+			{{ solveMessageTime }}
 		</div>
 		<div class="message-text" :class="messageTextClass">
 			<img v-if="messageData.imgUrl" :src="messageData.imgUrl" alt="img" />
@@ -21,7 +27,7 @@ const messageImageClass = props.messageData.imgUrl ? 'with-img' : 'without-img'
 			</p>
 		</div>
 		<div v-if="messageData.userId != userId" class="message-time">
-			{{ messageData.messageTime }}
+			{{ solveMessageTime }}
 		</div>
 	</div>
 </template>
